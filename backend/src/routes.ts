2310ed76d6fdf8a141c546a725e003cc69231f48
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-09-12 19:07:45 -0300

    cadastrando categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,8 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
+import { CreteCategoryController } from "./controllers/category/CreateCategoryController";
+
 import isAuthenticated from "./middlewares/isAuthenticated";
 
 const router = Router();
@@ -12,5 +14,12 @@ const router = Router();
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+// routes category
+router.post(
+  "/categories",
+  isAuthenticated,
+  new CreteCategoryController().handle
+);
 
 export default router;
