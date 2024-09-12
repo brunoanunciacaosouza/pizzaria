c13ebf70c7cfc15eecdfc23f3d3387632db24e05
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-09-12 18:48:22 -0300

    buscando detalhes dos usuarios

--- backend/src/controllers/user/DetailUserController.ts
+++ backend/src/controllers/user/DetailUserController.ts
@@ -5,7 +5,9 @@ class DetailUserController {
   async handle(req: Request, res: Response) {
     const detailUserService = new DetailUserService();
 
-    const user = await detailUserService.execute();
+    const user_id = req.user_id;
+
+    const user = await detailUserService.execute(user_id);
 
     return res.json(user);
   }
--- backend/src/services/user/DetailUserService.ts
+++ backend/src/services/user/DetailUserService.ts
@@ -1,9 +1,19 @@
 import prismaCliente from "../../prisma";
 
 class DetailUserService {
-  async execute() {
-    
-    return { ok: true };
+  async execute(user_id: string) {
+    const user = await prismaCliente.user.findFirst({
+      where: {
+        id: user_id,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return user;
   }
 }
 
