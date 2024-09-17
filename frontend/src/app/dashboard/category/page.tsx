e7eca47b142402eaba169a69737d2e4e86c2fe21
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-09-17 17:05:33 -0300

    cadastrando categorias

--- frontend/src/app/dashboard/category/page.tsx
+++ frontend/src/app/dashboard/category/page.tsx
@@ -1,12 +1,38 @@
 import { api } from "@/services/api";
 import Button from "../components/Button";
 import styles from "./styles.module.scss";
+import { getCookieServer } from "@/lib/cookieServer";
+import { redirect } from "next/navigation";
 
 export default function Category() {
+  async function handleRegisterCategory(formData: FormData) {
+    "use server";
 
-    async function handleRegisterCategory(formData: FormData){
-        "use server";
+    const name = formData.get("name");
+
+    if (name === "") {
+      return;
     }
+
+    const token = getCookieServer();
+
+    await api
+      .post(
+        "/category",
+        { name },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .catch((error) => {
+        console.log(error);
+        return;
+      });
+
+    redirect("/dashboard");
+  }
 
   return (
     <main className={styles.container}>
