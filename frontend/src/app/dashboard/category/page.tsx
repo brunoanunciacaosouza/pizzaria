86c7b271d9e855bd5c5211e8edbd95a4107cbd13
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-09-17 16:50:23 -0300

    criando pagina de categoria

---
+++ frontend/src/app/dashboard/category/page.tsx
@@ -0,0 +1,28 @@
+import { api } from "@/services/api";
+import Button from "../components/Button";
+import styles from "./styles.module.scss";
+
+export default function Category() {
+
+    async function handleRegisterCategory(formData: FormData){
+        "use server";
+    }
+
+  return (
+    <main className={styles.container}>
+      <h1>Nova Categoria</h1>
+
+      <form className={styles.form} action={handleRegisterCategory}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome da categoria, ex: Pizzas"
+          required
+          className={styles.input}
+        />
+
+        <Button name="Cadastrar" />
+      </form>
+    </main>
+  );
+}
