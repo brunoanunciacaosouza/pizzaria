c57988bdd033d590eb9d2351c7fce7939293ec3f
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-09-18 09:07:58 -0300

    cadastrando produto

--- frontend/src/app/dashboard/components/Form/index.tsx
+++ frontend/src/app/dashboard/components/Form/index.tsx
@@ -4,8 +4,20 @@ import { UploadCloud } from "lucide-react";
 import styles from "./styles.module.scss";
 import { ChangeEvent, useState } from "react";
 import Image from "next/image";
+import Button from "../Button";
+import { api } from "@/services/api";
+import { getCookieCliente } from "@/lib/cookieClient";
 
-export default function Form() {
+interface CategoryProps {
+  id: string;
+  name: string;
+}
+
+interface Props {
+  categories: CategoryProps[];
+}
+
+export default function Form({ categories }: Props) {
   const [image, setImage] = useState<File>();
   const [previewImage, setPreviewImage] = useState("");
 
@@ -25,11 +37,43 @@ export default function Form() {
     }
   }
 
+  async function handleRegisterProduct(formData: FormData) {
+    const categoryIndex = formData.get("category");
+    const name = formData.get("name");
+    const price = formData.get("price");
+    const description = formData.get("description");
+
+    if (!categoryIndex || !name || !price || !description || !image) {
+      return;
+    }
+
+    const data = new FormData();
+
+    data.append("name", name);
+    data.append("price", price);
+    data.append("description", description);
+    data.append("category_id", categories[Number(categoryIndex)].id);
+    data.append("file", image);
+
+    const token = getCookieCliente();
+
+    await api
+      .post("/product", data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        console.log(error);
+        return;
+      });
+  }
+
   return (
     <main className={styles.container}>
       <h1>Novo produto</h1>
 
-      <form className={styles.form}>
+      <form className={styles.form} action={handleRegisterProduct}>
         <label className={styles.labelImage}>
           <span>
             <UploadCloud size={24} color="#fff" />
@@ -53,6 +97,39 @@ export default function Form() {
             />
           )}
         </label>
+
+        <select name="category">
+          {categories.map((category, index) => (
+            <option key={category.id} value={index}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+
+        <input
+          type="text"
+          name="name"
+          placeholder="Digite o nome do produto"
+          required
+          className={styles.input}
+        />
+
+        <input
+          type="text"
+          name="price"
+          placeholder="Preço do produto"
+          required
+          className={styles.input}
+        />
+
+        <textarea
+          name="description"
+          className={styles.input}
+          required
+          placeholder="Digite a descrição do produto..."
+        ></textarea>
+
+        <Button name="Cadastrar Produto" />
       </form>
     </main>
   );
--- frontend/src/app/dashboard/product/page.tsx
+++ frontend/src/app/dashboard/product/page.tsx
@@ -1,9 +1,17 @@
+import { getCookieServer } from "@/lib/cookieServer";
 import Form from "../components/Form";
+import { api } from "@/services/api";
 
-export default function Product() {
+export default async function Product() {
+  const token = getCookieServer();
+  const response = await api.get("/category", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   return (
     <main>
-      <Form />
+      <Form categories={response.data} />
     </main>
   );
 }
