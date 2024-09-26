d25e9d9ac16ab3c1e51a59df1d43947d4ec530d5
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-09-26 13:24:32 -0300

    criando tela de login

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
 
@@ -5,5 +6,22 @@ export default function Routes() {
   const isAuthenticated = false;
   const loading = false;
 
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={60} color="#fff" />
+      </View>
+    );
+  }
+
   return isAuthenticated ? <AppRoutes /> : <AuthRoutes />;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1d1d2e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
