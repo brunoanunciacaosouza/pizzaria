873aaf861d5d84253dde34132eca85b00cf3f21b
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-09-26 15:32:32 -0300

    recebendo parametros

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -1,6 +1,18 @@
+import { RouteProp, useRoute } from "@react-navigation/native";
 import { StyleSheet, View, Text } from "react-native";
 
+type RouteDetailParams = {
+  Order: {
+    number: string | number;
+    order_id: string;
+  };
+};
+
+type OrderRouteProps = RouteProp<RouteDetailParams, "Order">;
+
 export default function Order() {
+  const route = useRoute<OrderRouteProps>();
+
   return (
     <View style={styles.container}>
       <Text>Tela de order</Text>
